4f8727bbf2b8cf8c99bd24def7fea620f0a8118f
Aktan A <aktan.amanaliev@gmail.com>
2024-06-18 10:04:32 +0600

    Add trip endpoints

--- src/main/java/com/neobis/neotour/repository/TripRepository.java
+++ src/main/java/com/neobis/neotour/repository/TripRepository.java
@@ -1,9 +1,15 @@
 package com.neobis.neotour.repository;
 
 import com.neobis.neotour.model.Trip;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TripRepository extends JpaRepository<Trip, Long> {
+    Page<Trip> findAllByDeletedFalse(Pageable pageable);
+    Optional<Trip> findByIdAndDeletedFalse(Long id);
 }
--- src/main/java/com/neobis/neotour/service/TripService.java
+++ src/main/java/com/neobis/neotour/service/TripService.java
@@ -1,4 +1,12 @@
 package com.neobis.neotour.service;
 
+import com.neobis.neotour.dto.TripDto;
+import org.springframework.data.domain.Page;
+
 public interface TripService {
+    TripDto getTripById(Long id);
+    Page<TripDto> getTrips(int page, int size);
+    TripDto createTrip(TripDto tripDto);
+    TripDto updateTripById(Long id, TripDto tripDto);
+    void deleteTripById(Long id);
 }
