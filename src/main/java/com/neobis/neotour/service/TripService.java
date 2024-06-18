d2a220d8674309bf86f78ec9e24a24f1b5370c68
Aktan A <aktan.amanaliev@gmail.com>
2024-06-18 17:21:42 +0600

    Add filtering trips by continent

--- src/main/java/com/neobis/neotour/service/TripService.java
+++ src/main/java/com/neobis/neotour/service/TripService.java
@@ -1,11 +1,12 @@
 package com.neobis.neotour.service;
 
 import com.neobis.neotour.dto.TripDto;
+import com.neobis.neotour.enums.Continent;
 import org.springframework.data.domain.Page;
 
 public interface TripService {
     TripDto getTripById(Long id);
-    Page<TripDto> getTrips(int page, int size);
+    Page<TripDto> getTrips(int page, int size, Continent continent);
     TripDto createTrip(TripDto tripDto);
     TripDto updateTripById(Long id, TripDto tripDto);
     void deleteTripById(Long id);
